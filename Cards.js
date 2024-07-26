7ac0cefd5502535d93c4d7f75d475e43240dbc51
SidarthVermaSecA <n01702596@humber.ca>
2024-07-26 15:43:22 -0400

    navbar

--- Cards.js
+++ Cards.js
@@ -1,116 +1,109 @@
-// const chatbotToggler = document.querySelector(".chatbot-toggler");
-// const closeBtn = document.querySelector(".close-btn");
-// const chatbox = document.querySelector(".chatbox");
-// const chatInput = document.querySelector(".chat-input textarea");
-// const sendChatBtn = document.querySelector(".chat-input span");
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
 
-// let userMessage = null; // Variable to store user's message
-// const API_KEY = "AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc"; // Paste your API key here
-// const inputInitHeight = chatInput.scrollHeight;
+let userMessage = null; // Variable to store user's message
+const API_KEY = "AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc"; // Paste your API key here
+const inputInitHeight = chatInput.scrollHeight;
 
-// const createChatLi = (message, className) => {
-//     // Create a chat <li> element with passed message and className
-//     const chatLi = document.createElement("li");
-//     chatLi.classList.add("chat", `${className}`);
-//     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-//     chatLi.innerHTML = chatContent;
-//     chatLi.querySelector("p").textContent = message;
-//     return chatLi; // return chat <li> element
-// }
+const createChatLi = (message, className) => {
+    // Create a chat <li> element with passed message and className
+    const chatLi = document.createElement("li");
+    chatLi.classList.add("chat", `${className}`);
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    chatLi.innerHTML = chatContent;
+    chatLi.querySelector("p").textContent = message;
+    return chatLi; // return chat <li> element
+}
 
-// const generateResponse = (chatElement) => {
-//     const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc";
-//     const messageElement = chatElement.querySelector("p");
+const generateResponse = (chatElement) => {
+    const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc";
+    const messageElement = chatElement.querySelector("p");
 
-//     // Define the properties and message for the API request
-//     const requestOptions = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//             "Authorization": `Bearer ${API_KEY}`
-//         },
-//         body: JSON.stringify({
-//             model: "gemini-1.5-flash-latest",
-//             messages: [{role: "user", content: userMessage}],
-//         })
-//     }
+    // Define the properties and message for the API request
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gemini-1.5-flash-latest",
+            messages: [{role: "user", content: userMessage}],
+        })
+    }
 
-//     // Send POST request to API, get response and set the reponse as paragraph text
-//     fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-//         messageElement.textContent = data.choices[0].message.content.trim();
-//     }).catch(() => {
-//         messageElement.classList.add("error");
-//         messageElement.textContent = "Oops! Something went wrong. Please try again.";
-//     }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-// }
+    // Send POST request to API, get response and set the reponse as paragraph text
+    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
+        messageElement.textContent = result.candidates[0].content.parts[0].text;
+    }).catch(() => {
+        messageElement.classList.add("error");
+        messageElement.textContent = "Oops! Something went wrong. Please try again.";
+    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
+}
 
-// const handleChat = () => {
-//     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-//     if(!userMessage) return;
+const handleChat = () => {
+    userMessage = chatInput.value.trim(); 
+    if(!userMessage) return;
+    chatInput.value = "";
+    chatInput.style.height = `${inputInitHeight}px`;
 
-//     // Clear the input textarea and set its height to default
-//     chatInput.value = "";
-//     chatInput.style.height = `${inputInitHeight}px`;
-
-//     // Append the user's message to the chatbox
-//     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-//     chatbox.scrollTo(0, chatbox.scrollHeight);
     
-//     setTimeout(() => {
-//         // Display "Thinking..." message while waiting for the response
-//         const incomingChatLi = createChatLi("Thinking...", "incoming");
-//         chatbox.appendChild(incomingChatLi);
-//         chatbox.scrollTo(0, chatbox.scrollHeight);
-//         generateResponse(incomingChatLi);
-//     }, 600);
-// }
+    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
+    chatbox.scrollTo(0, chatbox.scrollHeight);
+    
+    setTimeout(() => {
+        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        chatbox.appendChild(incomingChatLi);
+        chatbox.scrollTo(0, chatbox.scrollHeight);
+        generateResponse(incomingChatLi);
+    }, 600);
+}
 
-// chatInput.addEventListener("input", () => {
-//     // Adjust the height of the input textarea based on its content
-//     chatInput.style.height = `${inputInitHeight}px`;
-//     chatInput.style.height = `${chatInput.scrollHeight}px`;
-// });
+chatInput.addEventListener("input", () => {
+    chatInput.style.height = `${inputInitHeight}px`;
+    chatInput.style.height = `${chatInput.scrollHeight}px`;
+});
 
-// chatInput.addEventListener("keydown", (e) => {
-//     // If Enter key is pressed without Shift key and the window 
-//     // width is greater than 800px, handle the chat
-//     if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
-//         e.preventDefault();
-//         handleChat();
-//     }
-// });
+chatInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+        e.preventDefault();
+        handleChat();
+    }
+});
 
-// sendChatBtn.addEventListener("click", handleChat);
-// closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-// chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+sendChatBtn.addEventListener("click", handleChat);
+closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
+chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
 
-// // Replace 'YOUR_API_KEY' with your actual API key
-// const apiKey = 'AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc';
-// const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
+const apiKey = 'AIzaSyAQYMNgusd0dulb6qFmn-aSStXq8wGqesc';
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 
-// const data = {
-//   contents: [
-//     {
-//       parts: [
-//         {
-//           text: "What is a credit card",
-//         },
-//       ],
-//     },
-//   ],
-// };
+const data = {
+  contents: [
+    {
+      parts: [
+        {
+          text: "What is a credit card",
+        },
+      ],
+    },
+  ],
+};
 
-// fetch(url, {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((result) => {
-//     console.log(result.candidates[0].content.parts[0].text);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(data),
+})
+  .then((response) => response.json())
+  .then((result) => {
+    console.log(result.candidates[0].content.parts[0].text);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
